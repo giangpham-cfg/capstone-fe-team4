ec3767f03474f8802f2dfc1732ce914ebe9d5b82
giangpham-cfg <giang.pham1610@gmail.com>
2023-10-17 21:56:19 +0200

    update sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import MenuPic from "../../images/navbar chef.png";
+import { FiLogIn } from "react-icons/fi";
 
 export default function Home() {
+<<<<<<< Updated upstream
   return <div></div>;
+=======
+  const mealTime = ["BREAKFAST", "LUNCH", "DINNER", "DESSERT"];
+  const navigate = useNavigate();
+  return (
+    <div className="home-page-container">
+      <div className="mealtime-list-container">
+        <img src={MenuPic} alt="" width={70} />
+        <div className="type-container">
+          {mealTime.map((item, index) => (
+            <div
+              className="type"
+              key={index}
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate(`/types/${item}`)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+
+        <div className="submit" onClick={() => navigate("/submit")}>
+          <span>
+            <FiLogIn />
+          </span>
+          Submit Recipe
+        </div>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
